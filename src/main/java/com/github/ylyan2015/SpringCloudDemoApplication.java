1fac187c88d9768cddf23efa9d113e1437322f43
Yan Yulin <ylyan@mail.ustc.edu.cn>
2026-05-10 21:56:19 +0800

    feat: 提交demo代码

---
+++ src/main/java/com/github/ylyan2015/SpringCloudDemoApplication.java
@@ -0,0 +1,15 @@
+package com.github.ylyan2015;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SpringCloudDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemoApplication.class, args);
+    }
+}
